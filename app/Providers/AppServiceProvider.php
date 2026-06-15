5b67b40380cfe83d2742d23e92d364eb9f36bc07
ShurGith <esnola@gmail.com>
2026-06-15 22:46:25 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,29 +1,26 @@
 <?php
-
-namespace App\Providers;
-
-use App\Models\User;
-use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\ServiceProvider;
-
-class AppServiceProvider extends ServiceProvider
-{
+  
+  namespace App\Providers;
+  
+  use App\Models\User;
+  use Illuminate\Support\Facades\Gate;
+  use Illuminate\Support\ServiceProvider;
+  
+  class AppServiceProvider extends ServiceProvider
+  {
     /**
      * Register any application services.
      */
     public function register(): void
     {
-        //
+      //
     }
-
+    
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        Gate::define(
-            'view-orders',
-            fn (User $user): bool => (bool) $user->is_admin
-        );
+      Gate::define('view-orders', fn(User $user): bool => (bool)$user->is_admin);
     }
-}
+  }
